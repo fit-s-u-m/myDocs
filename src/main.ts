974e65246b714428c4f4a737cd81de13db10ea41
fit-s-u-m <nehemiahmaths@gmail.com>
2025-05-11 00:18:52 +0300

    fix: remove unused variables

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+// import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { MyScene } from './scene';
